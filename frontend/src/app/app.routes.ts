6d07a7907a89dc9b28d015716f16231fbcef646f
NielsLazaroms <niels.lazaroms@live.nl>
2026-03-03 20:05:03 +0100

    Refactor application structure and integrate Tailwind CSS.
    - Moved source files to new `frontend` directory for better organization.
    - Removed placeholder content from `app.html` and simplified the template structure.
    - Added Tailwind CSS with PostCSS integration into the project.
    - Updated `package.json` with `TailwindCSS`, `postcss`, and `mn-angular-lib` dependencies.

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import {LoginPage} from './pages/login-page/login-page';
+
+export const routes: Routes = [
+  { path: 'login', component: LoginPage},
+  {path: '**', redirectTo: '/login'},
+];
